4c73ab0a1e0a3d1f2b73e454db0681afabe071c3
Jena Krkbesevic <jenakrkbesevic@jenas-air.mynetworksettings.com>
2024-04-14 22:39:51 -0400

    worked

---
+++ src/movie.js
@@ -0,0 +1,10 @@
+function Movie() {
+    return (
+        <div className="Movie">
+            <h2>Movie Title: The Batman</h2>
+            <h4>Director: Matt Reeves</h4>
+            <p>Stars: Robert Pattinson, Zoe Kravitz</p>
+        </div>
+    );
+}
+export default Movie;
